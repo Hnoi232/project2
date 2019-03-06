f5fc48e788f64ba62cee12a8fb2df8bb19a4f645
unknown <muhayahana@gmail.com>
2019-03-06 20:52:10 +0300

    HanaProject2

--- js/app.js
+++ js/app.js
@@ -1,15 +1,14 @@
 
 //initialize api sources
-//BBC News from Google Api news
-var bbc ="https://newsapi.org/v2/everything?sources=bbc-news&apiKey=dff2ba8d21e44c239281120099ade122";
 //New York Time from official website
-var nyt="https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=LmXMOZktAei7mFAgA2SS4VetJDAVNV9t";
+var nyt="https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=LmXMOZktAei7mFAgA2SS4VetJDAVNV9t&fl=40";
+//BBC News from Google Api news
+var bbc ="https://newsapi.org/v2/everything?pageSize=100&sources=bbc-news&apiKey=dff2ba8d21e44c239281120099ade122";
 //Google news from Google API news
 var googlenews="https://newsapi.org/v2/everything?pageSize=100&q=all&apiKey=dff2ba8d21e44c239281120099ade122";
-
 // Initialize varialbes
 var main = document.getElementById("main");
-
+var search="";
 var source1=[];
 var source2=[];
 var source3=[];
@@ -34,10 +33,19 @@ LoadPage(LoadSource,index);
 
 $("#wait").hide();
 $("#btn").css('visibility', 'visible');
+}).catch(function(err) {
+errormsg();
 });
 
+function errormsg()
+{
+	 alert("We can't connect to the server, please try again later");
+  main.innerHTML="<div style='position: absolute;top: 45%;left:40%;font-size:40px;'> 404 Not Found </div> ";
+  $("#wait").hide();
+}
 function LoadPage(source,index)
 {
+	
 var last=index+20;
 if(source.length < last)
 {	
@@ -61,11 +69,39 @@ function clearmain()
 	index=0;
 }
 
-function getSource(src)
+function getSource(src,nmbr)
 {
-	clearmain();
+clearmain();
 $("#btn").css('visibility', 'hidden');
 $("#wait").show();
+main.innerHTML="";
+	if(src.length==0)
+	{
+			promise(function(){
+					switch(src){
+		case 1:$.get(nyt,function(data){ajaxCallback(data,1)});
+		break;
+		case 2:$.get(bbc,function(data){ajaxCallback(data,2)});
+		break;
+		case 3:$.get(googlenews,function(data){ajaxCallback(data,3)});		
+		break;
+		default:location.reload();
+		break;
+		}
+		
+	
+			}).then(function(){	switch(src){
+		case 1:src=source1;
+		break;
+		case 2:src=source1;
+		break;
+		case 3:src=source1;		
+		break;
+		default:location.reload();
+		break;
+		}}).catch(errormsg());
+	}
+	
 
 setTimeout(function(){
 	LoadSource=src;
@@ -73,12 +109,14 @@ LoadPage(LoadSource,index);
 $("#btn").css('visibility', 'visible');
 $("#wait").hide();
 },300);
-}
+	}
+			
 
-$("#source1").click(function(){getSource(source1)});
-$("#source2").click(function(){getSource(source2)});
-$("#source3").click(function(){getSource(source3)});
-$("#home").click(function(){getSource(sourceAll)});
+
+$("#source1").click(function(){getSource(source1,1)});
+$("#source2").click(function(){getSource(source2,2)});
+$("#source3").click(function(){getSource(source3,3)});
+$("#home").click(function(){getSource(sourceAll,0)});
 
 $("#btn").click(function(){LoadPage(LoadSource,index)});
 
@@ -92,17 +130,17 @@ $("#btn").click(function(){LoadPage(LoadSource,index)});
 		var title = article.title;
 		var url = article.url;
 		var idAr="id"+count.toString();
+		var views = count;
 		if(source==1){
 		var imgsrc = article.media[0]["media-metadata"][0].url;
 		var categ = article.section;
-		var views = article.published_date;
+		
 		var content=article.abstract;
 		}
 		else
 		{
 		var imgsrc = article.urlToImage;
 		var categ = article.description.split(",")[0];
-		var views = article.publishedAt;
 		var content=article.content;
 		}
 		
@@ -181,6 +219,33 @@ var result=$.grep(fullAtricleMap, function(el){ return el.id == target.id; });
 $('#popUp').show();
 }
 });
+$("#search a").click(function(){
+$("#search").toggleClass("active");
+});
+
+ 
+$('#search input').keypress(function (e) {
+
+$("#btn").css('visibility', 'hidden');
+var newsrc=LoadSource;
+ var key = e.which;
+   clearmain();
+  if(key == 13)  // the enter key code
+  {
+   $("#search").toggleClass("active");
+   search="";
+   $("#btn").css('visibility', 'visible');
+
+  }
+  else
+  {
+   search=document.querySelector("#search input").value;//String.fromCharCode(key);
+   var result=$.grep(fullAtricleMap, function(e){ return e => e.getElementsByClassName('articleContent')[0].firstChild.textContent.toUpperCase().includes(search.toUpperCase())});
+
+   LoadPage(newsrc.filter(e => e.getElementsByClassName('articleContent')[0].firstChild.textContent.toUpperCase().includes(search.toUpperCase())),0);
+   console.log(newsrc.filter(e => e.getElementsByClassName('articleContent')[0].firstChild.textContent.toUpperCase().includes(search.toUpperCase())));
+  }
+	  
 
 
-
+});   
